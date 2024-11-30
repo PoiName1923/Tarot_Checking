bf96c1724f156c1333e4906871e70a91bbf0240b
NguyenDinhTien <nguyendinhtien23012004@gmail.com>
2024-11-30 17:56:04 +0700

    Add 2 method

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from predict import tarot_three_card,tarot_one_card,tarot_celtic_cross
+
+
+st.title("TAROT FREE HERE")
+
+name_c = st.text_input("Enter your name: ")
+age_c = st.number_input("Enter your age:", min_value=1, max_value=120, value=2)
+rand_num = st.number_input("Enter random number: ", value = 3)
+
+if name_c and age_c >0 :
+    if st.button("Three Cards Telling"):
+        times, fortune, name, images = tarot_three_card(name_c, age_c, rand_num)
+        col1, col2, col3 = st.columns(3)
+        
+        with col1:
+            st.image(images[0], caption=name[0], use_container_width=True)
+            st.write(f'Your {times[0]} is {name[0]}.')
+            st.write(f'Fortune {fortune[0]}.')
+        with col2:
+            st.image(images[1], caption=name[1], use_container_width=True)
+            st.write(f'Your {times[1]} is {name[1]}.')
+            st.write(f'Fortune{fortune[1]}.')
+        with col3:
+            st.image(images[2], caption=name[2], use_container_width=True)
+            st.write(f'Your {times[2]} is {name[2]}.')
+            st.write(f'Fortune {fortune[2]}.')
+    if st.button("One Cards Telling"):
+        name, fortune, keyword, light_mean, shadow_mean, image = tarot_one_card(name_c, age_c, rand_num)
+        
+        st.image(image, caption=name, use_container_width=True)
+        st.write(f'Name: {name}')
+        st.write(f'Fortune: {fortune}')
+        st.write(f'Keywords: {keyword}')
+        st.write(f'Light Meaning: {light_mean}')
+        st.write(f'Shadow Meaning: {shadow_mean}')
+        
+    if st.button("Celtic Cross Telling"):
+        result = tarot_celtic_cross(name_c, age_c, rand_num)
+        
+        cols1 = st.columns(5)
+        for i, (pos,card) in enumerate(result.items()):
+            if i < 5:
+                with cols1[i]:
+                    st.image(card['img'], caption=card['name'], use_container_width=True)
+                    st.write(f'Position: {pos}')
+                    st.write(f'Name: {card['name']}')
+                    st.write(f'Fortune: {card['fortune']}')
+                    st.write(f'Suit: {card['suit']}')
+                    st.write(f'Keywords: {card['keywords']}')
+                    st.write(f'Light Meaning: {card['meanings_light']}')
+                    st.write(f'Shadow Meaning: {card['meanings_shadow']}')
+        cols2 = st.columns(5)
+        for i, (pos,card) in enumerate(result.items()):
+            if i >= 5:
+                with cols2[i-5]:
+                    st.image(card['img'], caption=card['name'], use_container_width=True)
+                    st.write(f'Position: {pos}')
+                    st.write(f'Name: {card['name']}')
+                    st.write(f'Fortune: {card['fortune']}')
+                    st.write(f'Suit: {card['suit']}')
+                    st.write(f'Keywords: {card['keywords']}')
+                    st.write(f'Light Meaning: {card['meanings_light']}')
+                    st.write(f'Shadow Meaning: {card['meanings_shadow']}')
--- predict.py
+++ predict.py
@@ -1,34 +1,81 @@
-# Import library
+# Import libraries
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
 import cv2
 import os
+from PIL import Image
+
 # Prepare
 data_dir = 'D:\\Python\\Tarot_Project\\data\\cards'
-files = os.listdir(data_dir)
-data = pd.read_json('D:\\Python\\Tarot_Project\\data\\tarot-images.json')
-data = data['cards']
+data = pd.read_json('D:\\Python\\Tarot_Project\\data\\tarot-images.json')['cards']
 data = pd.json_normalize(data)
-# Predict
-def tarot_predict():
-    index = random.sample(range(78),k = 3)
-    fortune = []
-    name = []
-    file = []
-    for i in range(3):
-        fortune += [data.loc[index[i],'fortune_telling'][0]]
-        name += [data.loc[index[i],'name']]
-        file += [data.loc[index[i],'img']]
 
-    times = ['Past','Present','Future']
-    f, ax = plt.subplots(1,3,figsize=(16,8))
-    print()
-    for i in range(3):
-        img = cv2.imread(os.path.join(data_dir,file[i]))
-        ax[i].imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
-        ax[i].set_xticks([])
-        ax[i].set_yticks([])
-        ax[i].set_title(name[i])
-        print(f'Your \033[1m{times[i]}\033[0m is \033[1m{name[i]}.\033[0m \033[1m{fortune[i]}.\033[0m')
+# Helper function to get a random card element
+def get_random_element(card_data, key):
+    return card_data[key][random.choice(range(len(card_data[key])))]
 
+# Function to display a single tarot card
+def tarot_one_card(name_char, age, rand_num):
+    roll = len(name_char.strip()) + age + rand_num
+    for i in range(roll):
+        index = random.randint(0, 77)  # Random index from 0 to 77    
+    
+    # Get card details
+    card = data.iloc[index]
+    name = card['name']
+    fortune = get_random_element(card, 'fortune_telling')
+    keyword = get_random_element(card, 'keywords')
+    light_mean = get_random_element(card, 'meanings.light')
+    shadow_mean = get_random_element(card, 'meanings.shadow')
+    file = card['img']
+    image = Image.open(os.path.join(data_dir, file))
+    # Return
+    return name, fortune, keyword, light_mean, shadow_mean, image
+# Function for a 3-card spread
+def tarot_three_card(name_char, age, rand_num):
+    roll = len(name_char.strip()) + age + rand_num
+    for i in range(roll):
+        index = random.sample(range(22), 3)  # Get 3 random Major Arcana cards
+        
+    # Prepare data for Major Arcana cards
+    major_arcana = data[data['arcana'] == 'Major Arcana'][['name', 'fortune_telling', 'img']]
+    
+    # Get details for each card
+    fortune, name, file = [], [], []
+    for idx in index:
+        card = major_arcana.iloc[idx]
+        fortune.append(get_random_element(card, 'fortune_telling'))
+        name.append(card['name'])
+        file.append(card['img'])
+    
+    # Display images and results
+    times = ['Past', 'Present', 'Future']
+    images = [Image.open(os.path.join(data_dir, f)) for f in file]
+    
+    return times, fortune, name, images
+
+# Function for a Celtic Cross spread (example template)
+def tarot_celtic_cross(name_char, age, rand_num):   
+    roll = len(name_char.strip()) + age + rand_num
+    for i in range(roll):
+        index = random.sample(range(78), k=10)
+    
+    positions = ['Present', 'Challenge', 'Past', 'Future', 'Above', 'Below','Advice', 'External Influences', 'Hopes and Fears', 'Outcome']
+    result = {}
+    # Lặp qua các vị trí và lấy thông tin từ các lá bài đã rút
+    for i, pos in enumerate(positions):
+        ind = index[i]
+        card = data.iloc[ind]
+        
+        result[pos] = {
+            'name': card['name'],
+            'fortune': get_random_element(card,'fortune_telling'),
+            'suit': card['suit'],
+            'keywords':  get_random_element(card,'keywords'),
+            'meanings_light':get_random_element(card,'meanings.light'),
+            'meanings_shadow':get_random_element(card,'meanings.shadow'),
+            'img': Image.open(os.path.join(data_dir,card['img']))
+        }
+    
+    return result
